2e8cda5a771d33eaeefb3e2324158beb48f2ac7b
Robert Davies <rob.davies1991@gmail.com>
2020-10-25 19:30:02 +0000

    updated nav with more options

--- includes/templates/nav.php
+++ includes/templates/nav.php
@@ -1,10 +1,12 @@
 <div class="container">
 <div class="nav-scroller py-1">
     <nav class="nav d-flex justify-content-between">
-      <a class="p-2 text-muted" href="#">Campaigns</a>
-      <a class="p-2 text-muted" href="#">Characters</a>
-      <a class="p-2 text-muted" href="#">Classes</a>
-      <a class="p-2 text-muted" href="#">Spells</a>
+      <a class="p-2 text-muted" href="#">Character</a>
+      <a class="p-2 text-muted" href="#">Monsters</a>
+      <a class="p-2 text-muted" href="#">Quests</a>
+      <a class="p-2 text-muted" href="#">Equipment</a>
+      <a class="p-2 text-muted" href="#">Blacksmith</a>
+      <a class="p-2 text-muted" href="#">Settings</a>
     </nav>
   </div>
 
