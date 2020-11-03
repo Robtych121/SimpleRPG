81436c2c3678a9ca971d52fd45da80aeb236cd53
Robert Davies <rob.davies1991@gmail.com>
2020-11-03 21:35:49 +0000

    cleaned index and nav

--- includes/templates/nav.php
+++ includes/templates/nav.php
@@ -1,11 +1,14 @@
 <div class="container">
 <div class="nav-scroller py-1">
     <nav class="nav d-flex justify-content-between">
-      <a class="p-2 text-muted" href="#">Character</a>
-      <a class="p-2 text-muted" href="#">Monsters</a>
+      <a class="p-2 text-muted" href="index.php">Home</a>
+      <a class="p-2 text-muted" href="#">Travel</a>
+      <a class="p-2 text-muted" href="#">Inventory</a>
+      <a class="p-2 text-muted" href="#">Town</a>
       <a class="p-2 text-muted" href="#">Quests</a>
-      <a class="p-2 text-muted" href="#">Equipment</a>
-      <a class="p-2 text-muted" href="#">Blacksmith</a>
+      <a class="p-2 text-muted" href="#">Jobs</a>
+      <a class="p-2 text-muted" href="#">Crafting</a>
+      <a class="p-2 text-muted" href="#">Store</a>
       <a class="p-2 text-muted" href="profile.php">Settings</a>
     </nav>
   </div>
--- index.php
+++ index.php
@@ -11,24 +11,10 @@ LoggedOutRedirect();
 ?>
 
 <div class="mainContent container">
-<div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-    <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
-    </div>
-  </div>
+<div class="message-box"></div>
     <div class="row">
         <div class="col-12">
-        <div class="message-box">Make your move!</div>
-        <div class="add-damage btn" type="button" onclick="removeHealth()">Damage</div>
-        <div class="add-heal btn" type="button" onclick="addHealth()">Heal</div>
-            <p>Homepage</p>
-            <?php
-            if(IsLoggedIn() === true){
-                echo 'You are logged in';
-            }
-            ?>
+          <p>Homepage</p>
         </div>
     </div>
 </div>
